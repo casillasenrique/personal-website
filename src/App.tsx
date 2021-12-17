7b5029e2d7f6adf6c25e5317f0afa5c071841488
Enrique <enriquec@mit.edu>
2021-12-16 22:41:13 -0500

    Restored version 1.0.0 on main branch

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import FrontPage from './pages/FrontPage';
+
+const VERSION: string = '1.0.0';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage version={VERSION} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SubwayPath.tsx
@@ -0,0 +1,47 @@
+import React, { FunctionComponent, useState } from 'react';
+import './SubwayPath.css';
+
+import { useSpring, animated, config } from 'react-spring';
+
+type SubwayPathProps = {
+  name: string;
+  color: string;
+  data: string;
+};
+
+const SubwayPath: FunctionComponent<SubwayPathProps> = ({
+  name,
+  color,
+  data,
+}) => {
+  const [path_color, set_color] = useState(color);
+  const [clear, set_clear] = useState(false);
+  const { x } = useSpring({
+    reset: true,
+    from: { x: 0 },
+    x: 1,
+    delay: 20,
+    config: {
+      duration: 5000,
+    },
+  });
+
+  return (
+    <div className="SubwayPath-container">
+      <animated.svg
+        viewBox="0 0 3449 2443"
+        strokeDasharray={10000}
+        strokeDashoffset={x.to((x) => (1 - x) * 10000)}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke={path_color}
+        strokeWidth="20"
+        fill="none"
+      >
+        <path d={data} />
+      </animated.svg>
+    </div>
+  );
+};
+
+export default SubwayPath;
