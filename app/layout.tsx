fcf84de142f661124acb5437d80d1e6abf257cb6
Enrique <enriquec@mit.edu>
2023-03-12 14:44:29 -0400

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 
 export const metadata = {
   title: 'Enrique Casillas',
-  name: "Enrique Casillas' personal website",
+  abstract: "Enrique Casillas' personal website",
   description:
     'Contains projects and social media links from Enrique Casillas.',
 };
