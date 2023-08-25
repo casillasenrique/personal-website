2f42290a3291cc26661c70ba76fb905b9cfa548e
Enrique <enriquec@mit.edu>
2023-08-25 15:05:47 -0400

    Updated about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -7,11 +7,11 @@ const AboutMe = () => {
       <div className={styles.AboutMe}>
         <h2>About Me</h2>
         <p>
-          Hi! My name is Enrique Casillas. I am a 4th year undergraduate student
-          studying computer science and engineering at MIT. I enjoy learning
-          about the latest technologies that can be used to help the most amount
-          of people possible. I also greatly enjoy data visualization,
-          particularly if it relates to urban science.
+          Hi! My name is Enrique Casillas. I am graduate student studying
+          computer science and engineering at MIT. I am passionate about
+          learning about the latest technologies that can be used to help people
+          and bring awareness to many important issues. My primary focus is on
+          data analysis, visualization, and web development.
         </p>
         <p>Check out some of my past projects below!</p>
       </div>
